006243be39b32e3fe235204d6758dfaef0f4fb7d
fUuiyaa <kengwanitchitsanupong@gmail.com>
2024-12-06 15:35:13 +0700

    lab8_3 completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -11,3 +11,51 @@
 "Brook"
 "Jinbe"
 */
+
+#include<iostream>
+using namespace std;
+int main(){
+
+
+    int age, height, bounty;
+    string character;
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age<=25){
+        cout << "Enter your height: ";
+        cin >> height;
+        if(height < 100){
+            character = "Chopper";
+        }
+        else if(height<180){
+            character = "Usopp";
+        }
+        else{
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            if(bounty <= 1100000000){
+            character = "Sanji";
+            } else {
+            character = "Zoro";
+            }
+        }
+    }
+    else if (age<=60){
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+        if(bounty<=500000000){
+            character = "Franky";
+        }
+        else{
+            character = "Jinbe";
+        }
+    } 
+    else {
+        character = "Brook";
+    }
+
+    cout << "Your character = " << character;
+
+    return 0;
+
+}
